140cdcbe5f86d8c501dde7b30e9a8f17c12798e7
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2019-11-19 20:14:19 +0300

    Fix package name references

--- blueprints/k5-ember-boilerplate/files/config/environment.js
+++ blueprints/k5-ember-boilerplate/files/config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   const ENV = {
     environment,
 
-    modulePrefix: 'wbc',
-    podModulePrefix: 'wbc/pods',
+    modulePrefix: '<%= dasherizedPackageName %',
+    podModulePrefix: '<%= dasherizedPackageName %/pods',
     rootURL: '/',
     locationType: 'auto',
     apiNamespace: process.env.EMBER_API_NAMESPACE || '',
@@ -26,15 +26,15 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-      LOG_RESOLVER: process.env.WBC_DEBUG_LOG_RESOLVER === 'true',
-      LOG_ACTIVE_GENERATION: process.env.WBC_DEBUG_LOG_ACTIVE_GENERATION === 'true',
-      LOG_TRANSITIONS: process.env.WBC_DEBUG_LOG_TRANSITIONS === 'true',
-      LOG_TRANSITIONS_INTERNAL: process.env.WBC_DEBUG_LOG_TRANSITIONS_INTERNAL === 'true',
-      LOG_VIEW_LOOKUPS: process.env.WBC_DEBUG_LOG_VIEW_LOOKUPS === 'true',
+      LOG_RESOLVER: process.env.EMBER_DEBUG_LOG_RESOLVER === 'true',
+      LOG_ACTIVE_GENERATION: process.env.EMBER_DEBUG_LOG_ACTIVE_GENERATION === 'true',
+      LOG_TRANSITIONS: process.env.EMBER_DEBUG_LOG_TRANSITIONS === 'true',
+      LOG_TRANSITIONS_INTERNAL: process.env.EMBER_DEBUG_LOG_TRANSITIONS_INTERNAL === 'true',
+      LOG_VIEW_LOOKUPS: process.env.EMBER_DEBUG_LOG_VIEW_LOOKUPS === 'true',
     },
 
     'ember-cli-mirage': {
-      enabled: process.env.WBC_MOCK_MIRAGE === 'true',
+      enabled: process.env.EMBER_MOCK_MIRAGE === 'true',
     },
   };
 
