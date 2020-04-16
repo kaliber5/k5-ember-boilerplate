eb36708907e13e185a0d1d459267428e54bdb8cf
simonihmig <simon.ihmig@kaliber5.de>
2020-04-16 11:39:53 +0200

    Remove extraneous files
    Fixes #12

--- blueprints/@kaliber5/k5-ember-boilerplate/index.js
+++ blueprints/@kaliber5/k5-ember-boilerplate/index.js
@@ -143,6 +143,9 @@ module.exports = {
       ['app/templates/application.hbs', 'app/pods/application/template.hbs'],
       ['tests/acceptance/steps/steps.js', 'steps.ts'],
       ['tests/helpers/flash-message.js', 'setup-flash-messages.ts'],
+      ['.travis.yml'],
+      ['tests/integration/steps/steps.js'],
+      ['tests/unit/steps/steps.js'],
     ].forEach(([filename, replacement]) => {
       const fullName = `${this.project.root}/${filename}`;
 
