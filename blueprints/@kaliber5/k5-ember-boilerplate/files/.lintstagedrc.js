c8f35d730277cc8ef2432ab681d36d54ef183629
simonihmig <simon.ihmig@kaliber5.de>
2020-09-07 12:56:03 +0200

    Fix lint-staged config
    `tsconfig-node.json` is not there anymore in the current setup, so this caused failures.

--- blueprints/@kaliber5/k5-ember-boilerplate/files/.lintstagedrc.js
+++ blueprints/@kaliber5/k5-ember-boilerplate/files/.lintstagedrc.js
@@ -6,12 +6,11 @@ module.exports = {
   // and TSC does not support providing a config and a path to a specific file at the same time.
   // Thus, two separate configs are used: one for Node, one for browser.
   // The function allows running the command globally, rather than once per each staged file.
-  '{app,mirage,tests,types}/**/*.ts': () => 'yarn lint:ts -p tsconfig.json',
-  '{*,.*,blueprints/*/*,config/**/*,lib/*/index,server/**/*}.{js,ts}': () => 'yarn lint:ts -p tsconfig-node.json',
+  '**/*.ts': 'yarn lint:ts',
 
   // Run ESLint, typescript-eslint and Prettier on staged files only
-  '**/*.{js,ts}': ['yarn lint:eslint --fix'],
+  '**/*.{js,ts}': 'yarn lint:eslint --fix',
 
   // Template lint
-  '**/*.hbs': ['yarn ember-template-lint'],
+  '**/*.hbs': 'yarn ember-template-lint',
 };
