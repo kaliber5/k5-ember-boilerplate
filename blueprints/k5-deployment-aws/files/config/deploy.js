d0ad066c069f67fcb8535881105a0b331e590155
simonihmig <simon.ihmig@kaliber5.de>
2020-11-24 10:54:18 +0100

    Fix AWS deployment

--- blueprints/k5-deployment-aws/files/config/deploy.js
+++ blueprints/k5-deployment-aws/files/config/deploy.js
@@ -13,6 +13,11 @@ module.exports = function (deployTarget) {
       // To disable ember-cli-deploy-cloudfront for only a particular environment, add
       // `ENV.pipeline.activateOnDeploy = false` to an environment conditional below.
       activateOnDeploy: true,
+      disabled: {
+        // The preview environment has separate S3 folders (see s3.prefix below) for each deployment, which have their own
+        // caching identity. Thus we don't need to invalidate the CloudFront cache.
+        cloudfront: deployTarget === 'preview',
+      },
     },
     cloudformation: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -34,7 +39,7 @@ module.exports = function (deployTarget) {
         return context.cloudformation.outputs.AssetsBucket;
       },
       prefix(context) {
-        return context.revisionData.revisionKey;
+        return deployTarget === 'preview' ? context.revisionData.revisionKey : '';
       },
       region: process.env.AWS_REGION,
       filePattern: '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,otf,wasm,json,html,mp4,webm,ogv}',
@@ -46,6 +51,10 @@ module.exports = function (deployTarget) {
         return context.cloudformation.outputs.CloudFrontDistribution;
       },
       region: process.env.AWS_REGION,
+      // we need to invalidate all files as not all are fingerprinted
+      objectPaths: ['/*'],
+      // wait for invalidation, to make sure our Lighthouse audits sees the new deployment
+      waitForInvalidation: true,
     },
     'revision-data': {
       type: 'git-commit',
