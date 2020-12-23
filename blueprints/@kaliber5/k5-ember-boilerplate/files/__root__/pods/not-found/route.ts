1c1f97c00d1c5271e8a487432f5cfd2c835fb6c8
simonihmig <simon.ihmig@kaliber5.de>
2020-12-23 11:21:26 +0100

    Fix native classes linting
    Fix linting errors of latest version of eslint-plugin-ember

--- blueprints/@kaliber5/k5-ember-boilerplate/files/__root__/pods/not-found/route.ts
+++ blueprints/@kaliber5/k5-ember-boilerplate/files/__root__/pods/not-found/route.ts
@@ -1,8 +1,8 @@
 import Route from '@ember/routing/route';
 import NotFoundError from '<%= dasherizedPackageName %>/utils/errors/not-found';
 
-export default Route.extend({
-  beforeModel() {
+export default class NotFoundRoute extends Route {
+  beforeModel(): void {
     throw new NotFoundError();
-  },
-});
+  }
+}
